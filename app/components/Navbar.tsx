d3ef42677c0601b7d5460c7d4a12aa19ab01c775
BENJYTESTA <amineibnlfassi2003@outlook.com>
2025-02-22 16:23:55 +0100

    init

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -46,7 +46,7 @@ export default function Navbar() {
       }`}
     >
       <div className="max-w-5xl mx-auto px-4 h-16 flex items-center justify-between">
-        <a 
+        <Link 
           href="/" 
           className="relative z-50 transition-transform hover:scale-105"
           aria-label="Home"
@@ -54,19 +54,19 @@ export default function Navbar() {
           <svg width="55" height="46" viewBox="0 0 55 46" fill="none" xmlns="http://www.w3.org/2000/svg" className="dark:fill-white fill-black">
             <path d="M0.43 21L9.61 -1.43051e-06H16.57L25.75 21H18.43L11.65 3.36H14.41L7.63 21H0.43ZM5.89 17.34L7.69 12.24H17.35L19.15 17.34H5.89ZM26.8799 21V-1.43051e-06H32.6999L41.0999 13.71H38.0399L46.1999 -1.43051e-06H52.0199L52.0799 21H45.6299L45.5699 9.72H46.5899L41.0099 19.11H37.8899L32.0699 9.72H33.3299V21H26.8799ZM2.71 46V25H9.79V46H2.71ZM13.1983 46V25H19.0183L29.6983 37.78H26.9983V25H33.8983V46H28.0783L17.3983 33.22H20.0983V46H13.1983ZM43.7896 32.8H53.1496V37.9H43.7896V32.8ZM44.2696 40.66H54.7696V46H37.3096V25H54.3796V30.34H44.2696V40.66Z" />
           </svg>
-        </a>
+        </Link>
 
         {/* Desktop Navigation */}
         <div className="hidden md:flex items-center space-x-8">
           {navLinks.map((link) => (
-            <a
+            <Link
               key={link.href}
               href={link.href}
               className="text-sm font-medium text-foreground/80 hover:text-primary transition-colors relative group"
             >
               {link.label}
               <span className="absolute -bottom-1 left-0 w-0 h-[2px] bg-primary transition-all group-hover:w-full" />
-            </a>
+            </Link>
           ))}
         </div>
 
